f9add239a73b606407c5f504691f74e6d0bb1b29
Ashwika071106-rgb <ashwikasumojay@gmail.com>
2021-03-06 12:47:10 +0530

    design for todo app

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {Text,View,TextInput,StyleSheet,TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style = {styles.parentContainer}>
+        <View style = {styles.headerContainer}>
+          <Text style = {styles.headerText}>Todo List</Text>
+        </View>
+        <View style = {styles.inputContainer}>
+          <TextInput
+              placeholder = "Type item here..."
+              style = {styles.inputBox}
+          >
+           </TextInput>
+           <TouchableOpacity style = {styles.addButton}>
+             <Text style = {{color: 'white'}}>ADD</Text>
+           </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  parentContainer : {
+    marginTop : 60
   },
-});
+  headerContainer : {
+    backgroundColor : '#37489e',
+  },
+  headerText : {
+    color : 'white',
+    padding: 20,
+    fontSize : 22,
+    fontWeight : 'bold',
+    textAlign : 'center'
+  },
+  inputContainer : {
+    marginTop : 20
+  },
+  inputBox : {
+    borderColor : 'black',
+    borderWidth : '2px',
+    borderRadius : 30,
+    height : 40
+  },
+  addButton : {
+    padding :10,
+    borderWidth : '2px',
+    backgroundColor : '#37489e',
+    width : '65px',
+    height : '40px',
+    margin : '20px',
+    borderRadius : 20,
+  },
+})
